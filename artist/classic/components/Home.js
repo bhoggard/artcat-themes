00fc4fa94fbf987de2151a6a15f90bebc6e1c75f
Barry Hoggard <barry@hoggard.org>
2019-10-19 22:57:35 -0400

    working fetch

---
+++ artist/classic/components/Home.js
@@ -0,0 +1,26 @@
+import { useQuery } from "@apollo/react-hooks"
+import { NetworkStatus } from "apollo-client"
+import gql from "graphql-tag"
+import ErrorMessage from "./ErrorMessage"
+import PostUpvoter from "./PostUpvoter"
+
+export const WEBSITE_QUERY = gql`
+  query {
+    website: websites_by_pk(id: 3) {
+      title
+      description
+      keywords
+    }
+  }
+`
+
+export default function Home() {
+  const { loading, error, data } = useQuery(
+    WEBSITE_QUERY
+  )
+  console.log(data)
+
+  if (error) return <ErrorMessage message="Error loading posts." />
+
+  return <h1>{data.website.title}</h1>
+}
